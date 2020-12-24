d57f19b7bfce2eb56afb2c9d695523a3d4954806
unknown <omprakash.om9@gmail.com>
2020-12-25 00:29:41 +0530

    end of section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+        }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad Requet",
+                401 => "error 1",
+                404 => "Not found",
+                _ => "Undefined error"
+            };
+
+        }
+    }
+}
