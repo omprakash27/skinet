e711e1b6648459831c37e18108d1451212f7b4ee
unknown <omprakash.om9@gmail.com>
2020-12-25 16:57:24 +0530

    End of section 6

--- Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductWithTypesAndBrandsSpecification.cs
@@ -6,10 +6,33 @@ namespace Core.Specifications
 {
     public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductWithTypesAndBrandsSpecification()
+        public ProductWithTypesAndBrandsSpecification(
+            ProductSpecParams productParams) : base(x=>
+            (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().ToLower().Contains(productParams.Search)) &&
+            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+            )
         {
             AddInclude(x => x.ProductBrand);
             AddInclude(x => x.ProductType);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex -1), productParams.PageSize);
+
+            if(!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch(productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(p => p.Name);
+                        break;
+                }
+            }
         }
 
         public ProductWithTypesAndBrandsSpecification(int id)
