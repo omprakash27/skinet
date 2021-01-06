754042fa923da807c4d8677d63299925a0e90121
unknown <omprakash.om9@gmail.com>
2021-01-07 01:06:47 +0530

    End of section 13

---
+++ Infrastructure/Data/BasketRepository.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using Core.Entitis;
+using Core.Interfaces;
+using StackExchange.Redis;
+using System.Text.Json;
+using System;
+
+namespace Infrastructure.Data
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDatabase _database;
+
+        public BasketRepository()
+        {
+        }
+
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+        }
+
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+           var data = await _database.StringGetAsync(basketId);
+
+           return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
+        }
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var create = await _database.StringSetAsync(basket.Id,
+             JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+
+            if(!create) return null;
+
+            return await GetBasketAsync(basket.Id);
+        }
+    }
+}
